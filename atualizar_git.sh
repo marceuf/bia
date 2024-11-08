9dd1409e34844b9359a18dac0f236ebc06b16f2c
Marcéu <teste@MacBookPro.bbrouter>
2024-11-08 00:12:11 -0300

    Atualização automática

---
+++ atualizar_git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Adiciona todas as modificações ao staging
+git add .
+
+# Mensagem de commit automática; substitua pelo que desejar
+mensagem_commit=${1:-"Atualização automática"}
+
+# Cria o commit com a mensagem
+git commit -m "$mensagem_commit"
+
+# Envia as modificações para o repositório remoto
+git push origin master
